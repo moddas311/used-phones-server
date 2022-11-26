c3b8d17b2948b635c531860bd9eb9ce200da21ca
moddas311 <moddasir311@gmail.com>
2022-11-26 16:27:40 +0600

    categories added

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middle wares
-app.get(cors());
-app.get(express.json());
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w89pmsb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('usedPhone').collection('category');
+        const phonesCollection = client.db('usedPhone').collection('usedPhones');
+
+
+        app.get('/category', async (req, res) => {
+            const query = {};
+            const category = await categoriesCollection.find(query).toArray();
+            res.send(category);
+        })
+    }
+    finally {
+
+    }
+} run().catch(er => console.log(er));
+
 
 app.get('/', (req, res) => {
     res.send('Used product server running');
